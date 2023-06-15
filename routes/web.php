1ef4371800589fcc45ef9499fa6730bed821133a
AriaAman <ariaaman@outlook.fr>
2023-06-15 18:15:43 +0200

    Test d'ajout de liens (routes)

--- routes/web.php
+++ routes/web.php
@@ -18,9 +18,22 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/blog', function(Request $request){
-    return [
-        "name" => $request ->input('name', 'Jhon Doe'),
-        "article" => "Article 1"
-    ];
+Route::prefix('/blog')->name("blog.")->group(function (){
+    Route::get('', function(Request $request){
+        return [
+            "link" => \route('blog.show', ['slug' => 'article', 'id' => 5 ]),
+        ];
+    })->name('index');
+
+    Route::get('/{slug}-{id}', function (string $slug, string $id) {
+        return [
+            "slug" => $slug,
+            "id" => $id
+        ];
+    })->where([
+        "id" => '[0-9]+',
+        "slug" => '[a-zA-Z0-9\-]+'
+    ])->name("show");
+
 });
+
